5395688a0ca5deaa6d57bd86b31b35f70e9aa398
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-26 22:10:03 +0200

    db

--- database/migrations/2016_04_26_203249_create_users_table.php
+++ database/migrations/2016_04_26_203249_create_users_table.php
@@ -1,6 +1,28 @@
 <?php
 
-use Illuminate\Database\Scheme\Blueprint:
-use Illuminate\Database\Migrations\Migration:
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table){
+            $table->increments('id');
+            $table->string("name");
+            $table->string("password");
+            $table->string("email");
+
+            $table->timestamps();
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('users');
+    }
+
+}
     
     
