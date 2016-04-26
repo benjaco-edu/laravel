d8107be958bbf154f484d2e2fda3621ecca16f14
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-26 22:35:25 +0200

    db

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,6 @@ class UsersTableSeeder extends Seeder{
      */
     public function run()
     {
-        // TODO: Implement run() method.
         DB::table('users')->delete();
 
         $users = array(
