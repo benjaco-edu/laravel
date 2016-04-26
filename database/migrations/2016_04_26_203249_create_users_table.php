bf210d7d783a0034e3c2424d06336b22749d0893
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-26 21:28:43 +0200

    Initial commit

---
+++ database/migrations/2016_04_26_203249_create_users_table.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Database\Scheme\Blueprint:
+use Illuminate\Database\Migrations\Migration:
+    
+    
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Todo app</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" type="text/css" />
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
