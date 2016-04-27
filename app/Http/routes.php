9f0801f57ce2d7cd087ec7ed72e6eb46771cd081
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-27 20:29:47 +0200

    middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,4 +17,4 @@ Route::get('/', array('as'=>'home', 'uses' => 'HomeController@getIndex'));
 
 Route::get('/login', array('as'=>"login", 'uses'=>'AuthController@getLogin'))->middleware('guest');
 
-Route::post('login', array('uses'=>'AuthController@postLogin'))->middleware("guest");
+Route::post('login', array('uses'=>'AuthController@postLogin'))->middleware("csrf");
