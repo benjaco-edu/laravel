f4ca4652281cc0944e907ee8befcd89fefb528fb
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-27 08:57:30 +0200

    loginforn

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', array('as'=>'home', 'uses' => 'HomeController@getIndex'));
+//Route::get('/', array('as'=>'home', 'uses' => 'HomeController@getIndex'));
 
 Route::get('/login', array('as'=>"login", 'uses'=>'AuthController@getLogin'));
-Route::post('login', array('uses'=>'AuthController@postLogin'));
+//Route::post('login', array('uses'=>'AuthController@postLogin'));
