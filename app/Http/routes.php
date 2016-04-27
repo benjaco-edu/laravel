506e4cb37c9c48816a98fe28c2162d68993dd783
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-27 20:15:21 +0200

    db

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,10 @@
 |
 */
 
+
+
 Route::get('/', array('as'=>'home', 'uses' => 'HomeController@getIndex'));
 
-Route::get('/login', array('as'=>"login", 'uses'=>'AuthController@getLogin'));
+Route::get('/login', array('as'=>"login", 'middleware'=>"Authenticate", 'uses'=>'AuthController@getLogin'));
 
 Route::post('login', array('uses'=>'AuthController@postLogin'));
