3165955f324e2f2615494a26a9c97cccd4128bd4
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-27 20:28:53 +0200

    middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,6 @@
 
 Route::get('/', array('as'=>'home', 'uses' => 'HomeController@getIndex'));
 
-Route::get('/login', array('as'=>"login", 'middleware'=>"guest", 'uses'=>'AuthController@getLogin'));
+Route::get('/login', array('as'=>"login", 'uses'=>'AuthController@getLogin'))->middleware('guest');
 
-Route::post('login', array('uses'=>'AuthController@postLogin'));
+Route::post('login', array('uses'=>'AuthController@postLogin'))->middleware("guest");
