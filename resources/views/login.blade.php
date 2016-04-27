d84a13f0c2f79bd9e9aa609bad87ff7a51d644fb
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-27 17:36:23 +0200

    login error

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.main')
 
 @section('content')
-    @foreach($errors->all() as $error)
-        <p class="error">{{$error}}</p>
-    @endforeach
     {{ Form::open()  }}
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
