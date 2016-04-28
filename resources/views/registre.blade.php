98c2f6688b26b98a55643e052cc04d7c9b457a51
Benjamin Schultz Larsen <benjaco1996@gmail.com>
2016-04-28 18:56:50 +0200

    opret konto

--- resources/views/registre.blade.php
+++ resources/views/registre.blade.php
@@ -6,7 +6,7 @@
         <p class="error">{{$error}}</p>
     @endforeach
     {{ Form::open()  }}
-    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="username" placeholder="Username" value="{{old('username')}}">
     <input type="password" name="password" placeholder="Password">
     <input type="password" name="password_gentag" placeholder="Password igen">
     <input type="submit" value="Login">
